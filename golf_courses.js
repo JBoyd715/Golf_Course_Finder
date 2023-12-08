897769e9bd2c86d8c400f911dbeee3290d6cf89f
JBoyd715 <102570810+JBoyd715@users.noreply.github.com>
2023-12-08 01:06:41 -0500

    Working List, Needs updates and styling

--- golf_courses.js
+++ golf_courses.js
@@ -1,48 +1,76 @@
 
 window.addEventListener("load", () => {
+
+// Add buttons to select radius 
+// Add function/method to get current loc
+const lat= 39.85919;
+const lon=-83.03379;
+const rad = 50000;
+
+
+
+
     const golfCourseList = document.querySelector("#golfCourseList");
     console.log(golfCourseList);
   
     const loadButton = document.querySelector("#loadButton");
     console.log(loadButton);
+    // On button click load courses
     loadButton?.addEventListener("click", () => {
-      // This is where you would call the api
-      let lat= 39.85919;
-      let long=-83.03379;
-
-      map = new google.maps.Map(document.getElementById('map'),{
-        latitude: lat,
-        longitude:long,
-        zoom:15
-      });
-      var request ={
-        location: new google.maps.LatLng(lat,long),
-        radius:10000,
-        type:'establishment',
-        keyword: 'golf'
-
-      }
-
-      //The only way I could figure out how to get this request to work was by creating a map;
-
-      service = new google.maps.places.PlacesService(map);
-      service.nearbySearch(request, callback);
-
-  
-      
-      /*[...Array(30).keys()].forEach((el) => {
-        const newChild = document.createElement("div");
-        newChild.innerText = el;
-        golfCourseList.appendChild(newChild);
-      });*/
+      iMap();
+      search();
     });
-    function callback(results, status) {
+
+    // Function to display details of all results 
+    function callback(results, status,pagination) {
       if (status == google.maps.places.PlacesServiceStatus.OK) {
+        console.log(results);
         results.forEach((el) => {
             const newChild = document.createElement("div");
-            newChild.innerText = el;
+            newChild.innerText = el['name'];
+           
+            // add selector of what type of golf (Disc,Normal)
+
+         /*   if(!el['name'].includes("Disc")){
             golfCourseList.appendChild(newChild);
+            } */
+            
+            golfCourseList.appendChild(newChild);
+
+            
         });
+        /* checks to see if the search results exceed 20 results. Each page only holds 20.
+        To get the rest of your results you have to use the next_page_token which is stored in pagination.
+        The .nextPage() method displays the rest of the results. 
+
+        */
+        if(pagination.hasNextPage){
+          pagination.nextPage();
+        }
       }
     }
+    // Fills out request parameters and then sends the request
+    function search(){
+    var request ={
+      location: new google.maps.LatLng(lat,lon),
+      radius:rad,
+      //type is establishment to get all businesses and keyword is golf to select all items related to golf
+      type:'establishment',
+      keyword: 'golf',
+    }
+    
+     //Call to Places API using nearbySearch
+      service.nearbySearch(request, callback);
+  }
+
+  //Initializes map so you can use the Places API
+  function iMap(){
+    map = new google.maps.Map(document.getElementById('map'),{
+      center: { lat: lat, lng: lon },
+      zoom:15
+    });
+    service = new google.maps.places.PlacesService(map);
+    
+    
+  }
   });
